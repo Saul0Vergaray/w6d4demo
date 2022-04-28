0b5460754ffa8f172b32bd8e15ca4e5804b9a13d
Saul Vergaray <saulvergaray99@gmail.com>
2022-04-28 14:13:59 -0600

    soup

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ var rollbar = new Rollbar({
 });
 
 // record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
 // record a generic message and send it to Rollbar
 
 const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
+    rollbar.log("Hello world!");
 })
 
 const port = process.env.PORT || 4545
