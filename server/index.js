0e661cd4676700d89a735385087acfc1f9608f54
Saul Vergaray <saulvergaray99@gmail.com>
2022-04-28 12:25:52 -0600

    pushing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../index.html'))
 })
+
 const port = process.env.PORT || 4545
+
 app.listen(port, () => console.log('beam us up ${port}!'))
