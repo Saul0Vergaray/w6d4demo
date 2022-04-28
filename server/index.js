c92c157689d2d275f654322ab6e030ef6271125b
Saul Vergaray <saulvergaray99@gmail.com>
2022-04-28 14:06:05 -0600

    w6

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const path = require('path')
-const app = express()
 
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: 'fcd6fd019ed846d3b2794dbe9d731107',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+// record a generic message and send it to Rollbar
+
+const app = express()
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../index.html'))
+    res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
 const port = process.env.PORT || 4545
 
-app.listen(port, () => console.log('beam us up ${port}!'))
+app.listen(port, () => console.log(`beam us up ${port}!`))
